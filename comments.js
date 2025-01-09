1921d66553dffc44f5262bd2a824e5564a5c48bb
Elyor_Norbutaev <elyor6442921@gmail.com>
2025-01-09 05:40:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,45 +1,31 @@
 // Create web server
-const express = require('express');
-const app = express();
-app.use(express.json());
-const { comments } = require('./data.js');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
 
-// Get all comments
-app.get('/comments', (req, res) => {
-    res.json(comments);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/comments', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
 });
 
-// Get comment by id
-app.get('/comments/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const comment = comments.find((comment) => comment.id === id);
-    res.json(comment);
+app.post('/comments', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
+    });
+  });
 });
 
-// Create new comment
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comments.push(comment);
-    res.json(comment);
-});
-
-// Update comment
-app.put('/comments/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const comment = comments.find((comment) => comment.id === id);
-    comment.body = req.body.body;
-    res.json(comment);
-});
-
-// Delete comment
-app.delete('/comments/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = comments.findIndex((comment) => comment.id === id);
-    comments.splice(index, 1);
-    res.status(204).send();
-});
-
-// Start server
-app.listen(4001, () => {
-    console.log('Server started at http://localhost:4001');
-});
+app.listen(3000);
